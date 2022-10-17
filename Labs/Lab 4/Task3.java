6a3c23656371924eafe3f0419cc0349e08d5f3dd
Shahriar Khan Limon <55662632+ShahriarKhanLimon@users.noreply.github.com>
2022-10-17 15:16:04 +0600

    Update and rename Task3_19101444.java to Task3.java

--- Labs/Lab 4/Task3.java
+++ Labs/Lab 4/Task3.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Task3_19101444 {
+public class Task3 {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         System.out.println("Enter the amount of time quantum for Round Robin Scheduling : ");
